f113fadfeb7ba67cd281452edd3a585c6d01284a
sahil95A <sahilaswal98@gmail.com>
2026-09-19 22:04:12 +0530

    Exclude FlywayAutoConfiguration in socket-server main application

--- UberSocketServer/src/main/java/com/example/ubersocketserver/UberSocketServerApplication.java
+++ UberSocketServer/src/main/java/com/example/ubersocketserver/UberSocketServerApplication.java
@@ -8,6 +8,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @SpringBootApplication
 @EnableDiscoveryClient
 @EntityScan("com.example.uberprojectentityservice.models")
+@SpringBootApplication(exclude = {FlywayAutoConfiguration.class})
 public class UberSocketServerApplication {
 
     public static void main(String[] args) {
