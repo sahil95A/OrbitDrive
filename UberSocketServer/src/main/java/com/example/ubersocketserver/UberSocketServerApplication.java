08476753077299eb0901f16b3e4d0ef5c4055d61
sahil95A <sahilaswal98@gmail.com>
2026-09-20 00:17:02 +0530

    Fix socket-server configuration and imports

--- UberSocketServer/src/main/java/com/example/ubersocketserver/UberSocketServerApplication.java
+++ UberSocketServer/src/main/java/com/example/ubersocketserver/UberSocketServerApplication.java
@@ -1,13 +1,12 @@
 package com.example.ubersocketserver;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @EntityScan("com.example.uberprojectentityservice.models")
 @SpringBootApplication
-@EnableAutoConfiguration(excludeName = {"org.springframework.boot.autoconfigure.flyway.FlywayAutoConfiguration"})
+
 public class UberSocketServerApplication {
 
     public static void main(String[] args) {
